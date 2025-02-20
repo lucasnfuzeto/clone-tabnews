d7daf2f0cc00d9233d8fbdd8df56c3beca980371
Lucas Fuzeto <43737201+lucasnfuzeto@users.noreply.github.com>
2025-02-20 15:59:30 +0000

    feat: add user migration and test

---
+++ tests/integration/api/v1/users/post.test.js
@@ -0,0 +1,24 @@
+import database from "infra/database";
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("POST /api/v1/users", () => {
+  describe("Anonymous user", () => {
+    test("With unique and valid data", async () => {
+      await database.query({
+        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3);",
+        values: ["lucas1", "lucas@gmail.com", "senha123"],
+      });
+
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+      });
+      expect(response.status).toBe(201);
+    });
+  });
+});
--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,4 +1,6 @@
 import retry from "async-retry";
+import database from "infra/database.js";
+import migrator from "models/migrator.js";
 
 async function waitForAllServices() {
   await waitForWebServer();
@@ -19,8 +21,18 @@ async function waitForAllServices() {
   }
 }
 
+async function clearDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+async function runPendingMigrations() {
+  await migrator.runPendingMigrations();
+}
+
 const orchestrator = {
   waitForAllServices,
+  clearDatabase,
+  runPendingMigrations,
 };
 
 export default orchestrator;
